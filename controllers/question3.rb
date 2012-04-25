f406845ec18943d76264826039ad4912dc7142bf
Ed Halferty <edhalferty@gmail.com>
2012-04-25 04:30:50 -0400

    Changed question3.rb to return text

--- controllers/question3.rb
+++ controllers/question3.rb
@@ -2,13 +2,12 @@ require_relative '../helpers/question3.rb'
 
 get '/question3/:population' do		
 
-	@population = params[:population].to_i
-	@total, @numbers, @letters, @excess = calc(@population)
+	population = params[:population].to_i
+	total, numbers, letters, excess = calc(population)
 	
-	@pattern = patternify(@numbers, @letters)
+	pattern = patternify(numbers, letters)
 
-	@title = 'Question 3 - license plate calculation'	
-	erb :results	
+	pattern + "|" + total + "|" + excess
 end
 
 
