36e209ec78d6e19afb8a7d397fe9d6b3c2bfa23a
Ed Halferty <edhalferty@gmail.com>
2012-04-24 23:33:02 -0400

    Fixed a few things.

--- helpers/question3.rb
+++ helpers/question3.rb
@@ -1,5 +1,5 @@
 class LicensePlateCalculator
-	attr_accessor :population, :numbers, :letters
+	attr_accessor :population, :numbers, :letters, :plates, :excess
  
 	def initialize(population)
 		@population = population
@@ -9,22 +9,20 @@ class LicensePlateCalculator
 		# Iterate through the combinations
 		while plates < population do
 			if numbers == 0
-				@numbers = @letters + 1
-				@letters = 0
+				numbers = letters + 1
+				letters = 0
 			else
-				@numbers -= 1
-				@letters += 1
+				numbers -= 1
+				letters += 1
 			end
 		end
+		@plates = plates
+		@excess = plates - population
 	end
 
 	def plates
 		return	((@numbers == 0)? 1 : (10 ** @numbers)) *
 			((@letters == 0)? 1 : (26 ** @letters))
-	end
-	
-	def excess
-		return plates - population
 	end
 end
 
