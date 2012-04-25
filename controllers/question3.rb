65ed00379f35e383ac0f5c908382eebf766d2867
Ed Halferty <edhalferty@gmail.com>
2012-04-25 00:06:19 -0400

    Fixed a few things.

--- controllers/question3.rb
+++ controllers/question3.rb
@@ -5,13 +5,7 @@ get '/question3/:population' do
 	@population = params[:population].to_i
 	@total, @numbers, @letters, @excess = calc(@population)
 	
-	# Decides if a comma should appear in the pattern result,
-	# and if plural nouns should be used
-	@pattern =	((@numbers > 0)? (@numbers.to_s + " number" + 
-			((@numbers > 1)? "s" : "")) : "") +
-			(((@numbers > 0) and (@letters > 0))? ", " : "") +
-			((@letters > 0)? (@letters.to_s + " letter" +
-			((@letters > 1)? "s" : "")) : "")
+	@pattern = patternify(@numbers, @letters)
 
 	@title = 'Question 3 - license plate calculation'	
 	erb :results	
--- helpers/question3.rb
+++ helpers/question3.rb
@@ -1,3 +1,5 @@
+# Finds the smallest number of letters and numbers
+# that can represent a given population.
 def calc(population)
 	numbers, letters = 1, 0
 	while plates(numbers, letters) < population do
@@ -15,8 +17,34 @@ def calc(population)
 	
 end
 
+# Calculates the number of plates for a given number
+# of numbers and letters
 def plates(numbers, letters)
 	return	((numbers == 0)? 1 : (10 ** numbers)) *
 		((letters == 0)? 1 : (26 ** letters))
 end
+
+# Decides if a comma should appear in the pattern result,
+# and if plural nouns should be used
+def patternify(numbers, letters)
+	pattern = ""
+	if (numbers > 0) do
+		pattern += numbers.to_s + " number"
+		if (numbers > 1) do
+			pattern += "s"
+		end
+	end
+	if (numbers > 0 and letters > 0) do
+		pattern += ", "
+	end
+	if (letters > 0) do
+		pattern += letters.to_s + " letter"
+		if (letters > 1) do
+			pattern += "s"
+		end
+	end
+	return pattern
+end
+
+			
 
